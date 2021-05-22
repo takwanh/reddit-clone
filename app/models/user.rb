b60e0ec4e0e6ae6001e761ed8b2b776a3bc5d363
takwanh <nagasan81@outlook.jp>
2021-05-22 18:21:38 +0900

    improve UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :communities
   has_many :posts
+
+  validates_presence_of :first_name, :last_name, :username
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get "u/:username" => "public#profile", as: :profile
   resources :communities do
     resources :posts
   end
